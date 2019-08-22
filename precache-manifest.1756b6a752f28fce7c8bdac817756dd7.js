cc9a9e2c9166f8e87cba50558bc72c3f68d72f1e
unknown <USER@WS044-T.janus.local>
2019-08-22 18:19:29 +0700

    Updates

---
+++ precache-manifest.1756b6a752f28fce7c8bdac817756dd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a220e924d5a8701ffe085701a2cbf81",
+    "url": "/janus-worldwide-project-app/index.html"
+  },
+  {
+    "revision": "a5b67ac17445b0b6879d",
+    "url": "/janus-worldwide-project-app/static/css/2.cc034990.chunk.css"
+  },
+  {
+    "revision": "2edd2ae6518f659709a4",
+    "url": "/janus-worldwide-project-app/static/css/main.30f1cc7c.chunk.css"
+  },
+  {
+    "revision": "a5b67ac17445b0b6879d",
+    "url": "/janus-worldwide-project-app/static/js/2.64995e9a.chunk.js"
+  },
+  {
+    "revision": "2edd2ae6518f659709a4",
+    "url": "/janus-worldwide-project-app/static/js/main.a629fbe4.chunk.js"
+  },
+  {
+    "revision": "180ff0d02b1d82eb7eff",
+    "url": "/janus-worldwide-project-app/static/js/runtime~main.2a12825e.js"
+  }
+]);
